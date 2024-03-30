2be117e917f2aa67a49295d86644c53a7f22113d
hindtak <115263978+hindtak@users.noreply.github.com>
2024-03-30 15:37:06 +0100

    customer schema with Authentcation

--- server/Models/Customer.js
+++ server/Models/Customer.js
@@ -1,2 +1,27 @@
-console.log("helloo ")
-console.log("helloo ")
+const mongoose = require("mongoose");
+const SchemaCustomer = new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    dateofbirth: {
+        type: String,
+        required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    permiumstatus: {
+      type : Boolean,
+      default: false,
+    },
+    createdAt: { type: Date, default: Date.now() },
+  });
+ const CustomerModel = mongoose.model("Customer", SchemaCustomer);
+ module.exports = CustomerModel; 
