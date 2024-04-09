2b4e37fa57e08d5e8100df13e9621e40e36cc394
SAMIR EDDEBBARHI <samireddebbarhi@gmail.com>
2024-04-09 17:10:02 +0000

    updated name of variables environments

--- server/middlewares/verifyJwtCus.js
+++ server/middlewares/verifyJwtCus.js
@@ -3,7 +3,7 @@ const verifyJwtCustomer = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
   if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
   const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+  jwt.verify(token, process.env.TOKEN_CUSTOMER, (err, user) => {
     if (err) {
       res.sendStatus(403); //inavlid token
     }
