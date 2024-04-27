9e49a69723d3e4238d7dcf4007b5ce3c5274f9f1
hindtak <115263978+hindtak@users.noreply.github.com>
2024-04-27 21:16:19 +0100

    NB : sidebar without modification

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import SignIn from './pages/SignIn'
 
-export default function App() {
+import React from 'react';
+import './App.css';
+import Navbar from './admin/components/sidebar/Navbar';
+import { BrowserRouter as Router, Navigate, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Report';
+import Products from './pages/Products';
+
+function App() {
   return (
-    <div>
-     
-      <SignIn />
-    </div>
-  )
+    <>
+      <Router>
+        <Navbar />
+        <Navigate>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Navigate>
+      </Router>
+    </>
+  );
 }
+
+export default App;
