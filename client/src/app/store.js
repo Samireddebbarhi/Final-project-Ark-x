013dfb357a61ab79c4ed51df6cdc04d5a56f227f
SAMIR EDDEBBARHI <samireddebbarhi@gmail.com>
2024-05-01 21:55:49 +0100

    adding auth slice

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,12 +1,14 @@
-import {configureStore} from '@reduxjs/toolkit'
-import productReducer from "../features/product/productSlice"
-import uploadReducer from "../features/upload/uploadSlice"
-import addproductReducer from '../features/product/productSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import productReducer from "../features/product/productSlice";
+import uploadReducer from "../features/upload/uploadSlice";
+import authReducer from "../features/auth/authSlice";
+import addproductReducer from "../features/product/productSlice";
 
 export const store = configureStore({
-    reducer:{
-        product: productReducer,
-        upload: uploadReducer,
-        addproduct: addproductReducer, 
-    },
+  reducer: {
+    product: productReducer,
+    upload: uploadReducer,
+    addproduct: addproductReducer,
+    auth: authReducer,
+  },
 });
