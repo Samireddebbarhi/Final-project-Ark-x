d9823e3117fe2c179f14eec63658186951ef40b9
Mohamed ESSRHIR <messghir4@gmail.com>
2024-05-24 11:14:53 +0100

    Admin_auth

--- server/controllers/authControllers/Admin_auth.js
+++ server/controllers/authControllers/Admin_auth.js
@@ -1,63 +1,51 @@
 require("dotenv").config();
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const AdminModel = require("../../models/AdminModel.js");
 
-exports.login = async (req, res) => {
+/*exports.register = async (req, res) => {
+  try {
+    const admin = req.body;
+    if (!admin) {
+      res.status(400).send("No data provided");
+    } else {
+      let hashedPassword = await bcrypt.hash(admin.password, 10); //Encryption of password using  Bcrypt
+      const newAdmin = new AdminModel({
+        ...admin,
+        password: hashedPassword,
+      })
+        .save()
+        .then(() =>
+          res
+            .status(200)
+            .json({ Succefull_msg: `${admin.username} added Succefully` })
+        );
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};*/
+
+exports.login = (req, res) => {
   const { email, password } = req.body;
 
-  if (!email || !password) {
+  if (!email || !password)
     return res.status(400).json({ msg: "Missing fields" });
-  }
 
-  try {
-    const admin = await AdminModel.findOne({ email });
+  AdminModel.findOne({ email }).then(async (admin) => {
+    if (!admin) return res.status(400).json({ msg: "Invalid credentials" });
 
-    if (!admin || admin.password !== password) {
-      return res.status(400).json({ msg: "Invalid credentials" });
-    }
-
-<<<<<<< HEAD
-    /*await bcrypt.compare(password, admin.password).then((isMatch) => {
-      if (!isMatch) {
+    await bcrypt.compare(password, admin.password).then((isMatch) => {
+      if (!isMatch)
         return res.status(400).json({ msg: "error password, Try again !!" });
-      }*/
 
-    const token = jwt.sign({ InfoAdmin: admin }, process.env.TOKEN_ADMIN, {
-      expiresIn: "1h",
-    });
+      const token = jwt.sign({ InfoAdmin: admin }, process.env.TOKEN_ADMIN, {
+        expiresIn: "20m",
+      });
 
-    res.status(200).json({
-      Login_Success: true,
-      token: token,
-      admin: {
-        name: admin.name,
-        email: admin.email,
-        username: admin.username,
-        role: admin.role,
-        permissions: admin.permissions,
-      },
+      res
+        .status(200)
+        .send(`${admin.username} logged in with a token: ${token}`);
     });
   });
 };
-=======
-    const token = jwt.sign({ InfoAdmin: admin }, process.env.TOKEN_ADMIN, {
-      expiresIn: "1h",
-    });
-
-    return res.status(200).json({
-      Login_Success: true,
-      token: token,
-      admin: {
-        name: admin.name,
-        email: admin.email,
-        username: admin.username,
-        role: admin.role,
-        permissions: admin.permissions,
-      },
-    });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send("Server error");
-  }
-};
->>>>>>> 0d83a2d6c602e9ab54b4ddfe5687b0573d3c551e
